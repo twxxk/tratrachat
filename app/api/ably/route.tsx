f3e72dee9c6679fa665503e0c7c6f48afd0e6d0f
twk <iwasaki.teruyuki@gmail.com>
2023-12-01 14:48:05 +0900

    Replace <Head> with Meta

--- app/api/ably/route.tsx
+++ app/api/ably/route.tsx
@@ -7,7 +7,8 @@ export async function GET(request) {
     const client = new Ably.Realtime(process.env.ABLY_API_KEY);
     const tokenRequestData = await client.auth.createTokenRequest({ clientId: 'ably-nextjs-demo' });
 
-    console.log(`Request: ${JSON.stringify(tokenRequestData)}`)
+    // console.log(`Request: ${JSON.stringify(tokenRequestData)}`)
+    
     // https://blog.kimizuka.org/entry/2023/07/28/235041
     return Response.json(tokenRequestData, {
         status: 200,
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
-import Head from 'next/head'
+import { Metadata } from "next";
+
 import dynamic from 'next/dynamic';
 
 const Chat = dynamic(() => import('../components/Chat'), {
   ssr: false,
 })
 
+export const metadata: Metadata = {
+  title: "Tra Tra Chat - Realtime Chat with translations",
+};
+
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Realtime Chat App with Ably, NextJS and Vercel</title>
-        <link rel="icon" href="https://static.ably.dev/motif-red.svg?nextjs-vercel" type="image/svg+xml" />
-      </Head>
-
       <main>
         <h1 className="title">TraTra Chat</h1>
         {/* Limitation: Messages are not persistent. Users cannot see the message history. No way to know who is already joining. */}
