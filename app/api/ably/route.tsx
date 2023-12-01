5df693b83001ca3b8b858f785f40f007d1333ac6
twk <iwasaki.teruyuki@gmail.com>
2023-12-01 17:19:30 +0900

    Make the connection file const

--- app/api/ably/route.tsx
+++ app/api/ably/route.tsx
@@ -2,11 +2,10 @@ import Ably from "ably/promises";
 
 // export const runtime = 'edge'; // got error
 export const revalidate = 0;
+const client = new Ably.Realtime(process.env.ABLY_API_KEY);
 
 export async function GET(request) {
-    const client = new Ably.Realtime(process.env.ABLY_API_KEY);
     const tokenRequestData = await client.auth.createTokenRequest({ clientId: 'ably-nextjs-demo' });
-
     // console.log(`Request: ${JSON.stringify(tokenRequestData)}`)
     
     // https://blog.kimizuka.org/entry/2023/07/28/235041
