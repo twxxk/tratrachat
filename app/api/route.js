6bbac5e2dfad856c3997c3e60284dc529ab08352
twk <iwasaki.teruyuki@gmail.com>
2023-12-01 11:35:41 +0900

    disable cache

--- app/api/route.js
+++ app/api/route.js
@@ -1,9 +1,20 @@
 import Ably from "ably/promises";
 
+// export const runtime = 'edge';
+export const revalidate = 0;
+
 export async function GET(request) {
     const client = new Ably.Realtime(process.env.ABLY_API_KEY);
     const tokenRequestData = await client.auth.createTokenRequest({ clientId: 'ably-nextjs-demo' });
 
     console.log(`Request: ${JSON.stringify(tokenRequestData)}`)
-    return Response.json(tokenRequestData);
+    // https://blog.kimizuka.org/entry/2023/07/28/235041
+    return Response.json(tokenRequestData, {
+        status: 200,
+        headers: {
+          'Cache-Control': 'no-store',
+          'CDN-Cache-Control': 'no-store',
+          'Vercel-CDN-Cache-Control': 'no-store'
+        }
+    });
 }
