b93d97564a29b4f3969b4438e599dc5dfaa13698
twk <iwasaki.teruyuki@gmail.com>
2023-12-02 21:03:22 +0900

    initial commit after move

--- app/chat/[thread_id]/page.tsx
+++ app/chat/[thread_id]/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 
 import dynamic from 'next/dynamic';
 
-const Chat = dynamic(() => import('../components/Chat'), {
+const Chat = dynamic(() => import('../../../components/Chat'), {
   ssr: false,
 })
 
